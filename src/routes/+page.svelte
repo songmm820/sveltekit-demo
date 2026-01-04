68f1552ab3cddc2889b930fe4c6e4eb47c58698a
songmm <mmsong@yeah.net>
2026-01-04 22:24:11 +0800

    feat: demo init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+
 	let counter = $state<number>(0);
 
 	function add() {
 		counter++;
+	}
+
+	async function goToAboutPage() {
+		await goto(resolve('/about'));
+		console.log('已经跳转到了/about页面');
 	}
 
 	// 监听counter变化时
@@ -16,15 +24,22 @@
 	});
 </script>
 
-<h1 class="text-3xl font-bold text-center text-gray-800">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="p-6">
+	<h1 class="text-3xl font-bold text-center text-gray-800">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<p>Counter: {counter}</p>
-<button class="font-bold text-lg my-button" onclick={add}> ADD </button>
+	<p>Counter: {counter}</p>
+	<button class="font-bold text-lg my-button" onclick={add}> ADD</button>
+
+	<br />
+
+	<button class="mt-8 font-bold text-lg my-button" onclick={goToAboutPage}> Go to About Page</button>
+</main>
+
 
 <style lang="css">
-	@reference '#app.css';
-	.my-button {
-		@apply bg-blue-500 text-white px-4 py-2 rounded-md;
-	}
+    @reference '#app.css';
+    .my-button {
+        @apply bg-blue-500 text-white px-4 py-2 rounded-md;
+    }
 </style>
