20fd4885fa11c6a3a3f69f09d1fd003703063157
songmm <songmm@qingsongjituan.com>
2026-01-21 20:51:34 +0800

    fix: 修复报错

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,23 @@ import tailwindcss from '@tailwindcss/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), enhancedImages()]
+	plugins: [tailwindcss(), sveltekit(), enhancedImages()],
+
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					// 将 components 目录所有组件打包成一个chunk
+					if (id.includes('/src/lib/components/') || id.includes('/src/components/')) {
+						// 将这些文件打包到名为 "components" 的 chunk 中
+						return 'components';
+					}
+					// 第三方库
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				}
+			}
+		}
+	}
 });
