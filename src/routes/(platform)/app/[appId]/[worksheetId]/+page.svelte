c90cbaa16612476252129143b015e31a71dceb93
songmm <songmm@qingsongjituan.com>
2026-01-28 16:01:56 +0800

    feat: layout

--- src/routes/(platform)/app/[appId]/[worksheetId]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-</script>
-
-<main class="flex h-full w-full flex-col items-center justify-center gap-4 overflow-hidden p-6">
-	worksheet
-</main>
-
-<style lang="css">
-</style>
